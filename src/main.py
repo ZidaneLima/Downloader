b8b369c91c86761afa6b6cdcbf3306dbc5b20a28
Zidane Lima <zidanelima2000@gmail.com>
2025-09-30 19:03:59 -0300

    Ajuste fianis das funcionalidades e scripts

--- src/main.py
+++ src/main.py
@@ -15,14 +15,14 @@ def main():
             break
         print("\n Opção inválida. \nPor favor, escolha '1' para Vídeo ou '2' para Áudio.")
     
-        try:
-            if formato == "1":
-                downvideo(url)
-            else:
-                downaudio(url)
-            print("\n Download Finalizado com sucesso!")
-        except Exception as e:
-            print(f"Ocorreu um erro durante o download: {e}")
+    try:
+        if formato == "1":
+            downvideo(url)
+        else:
+            downaudio(url)
+        print("\n Download Finalizado com sucesso!")
+    except Exception as e:
+        print(f"Ocorreu um erro durante o download: {e}")
 
 if __name__ == "__main__":
     main()
